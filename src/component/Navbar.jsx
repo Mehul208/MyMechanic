b2780564ad9c37e68aa9ef43a3bbc47686dd9022
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-07 16:38:29 +0530

    Added Navigation Bar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 import menuIcon from "../images/icon.png";
+import { data } from "./extras/nav_data";
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
     return (
-        <div className="main-nav">
+        <nav className="main-nav">
             <div className="logo">
                 <h2>
                     <span>My</span>Mechanic
                 </h2>
             </div>
-            <div className="menu-icon">
+            <div
+                style={toggle ? { display: "block" } : { display: "none" }}
+                className="_mnvd"
+            >
+                <ul>
+                    {data.map((item, i) => (
+                        <li key={i}>
+                            <a href={item.link}>{item.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div
+                className="menu-icon"
+                onClick={() => {
+                    setToggle(!toggle);
+                }}
+            >
                 <img src={menuIcon} alt="" />
             </div>
-        </div>
+        </nav>
     );
 };
 
---
+++ src/component/extras/nav_data.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        title: "About us",
+        link: "#",
+    },
+    {
+        title: "Buissness",
+        link: "#",
+    },
+    {
+        title: "Register",
+        link: "#",
+    },
+    {
+        title: "Logout",
+        link: "#",
+    },
+];
