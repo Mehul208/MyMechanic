9544ae55dda56f0dc0dc98f52953ce8989a11815
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-12 11:42:08 +0530

    Made Navbar and footer global
    - Wrapped around a div with class wrapper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Layout from "./component/HomePage/Layout";
 import "./app.css";
+import Footer from "./component/Footer";
+import Navbar from "./component/Navbar";
+import ListServices from "./component/ServicesPage/ListServices";
 const App = () => {
     return (
-        <div>
-            <Layout />
-        </div>
+            <div className="wrapper">
+                <Navbar />
+                <Routes>
+            </div>
+            <Footer />
+        </BrowserRouter>
     );
 };
 
