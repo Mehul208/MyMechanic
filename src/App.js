9284063b4622dde790c7fcba2df7c6264d4e02cf
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-12 11:42:25 +0530

    Added Navigation Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./component/HomePage/Layout";
 import "./app.css";
+import Page404 from "./Page404";
+import SignIn from "./component/AuthPages/SignIn";
+import SignUp from "./component/AuthPages/SignUp";
 import Footer from "./component/Footer";
 import Navbar from "./component/Navbar";
 import ListServices from "./component/ServicesPage/ListServices";
+import ServiceDetail from "./component/ServicesPage/ServiceDetail";
 const App = () => {
     return (
+        <BrowserRouter>
             <div className="wrapper">
                 <Navbar />
                 <Routes>
+                    <Route path="/" element={<Layout />} />
+                    <Route path="services" element={<ListServices />} />
+                    <Route path="services/:id" element={<ServiceDetail />} />
+                    <Route path="auth">
+                        <Route index element={<SignIn />} />
+                        <Route path="signUp" element={<SignUp />} />
+                        <Route path="*" element={<Page404 />} />
+                    </Route>
+                    <Route path="404" element={<Page404 />} />
+                    <Route path="*" element={<Page404 />} />
+                </Routes>
             </div>
             <Footer />
         </BrowserRouter>
