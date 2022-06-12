7e876c06733e7d535d2b2b0d53ac44ceed4af80a
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-12 11:37:51 +0530

    Removed navbar and footer

--- src/component/HomePage/Layout.jsx
+++ src/component/HomePage/Layout.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Footer from "../Footer";
-import Navbar from "../Navbar";
 import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
@@ -9,14 +7,10 @@ import Section4 from "./Section4";
 const Layout = () => {
     return (
         <div>
-            <div className="wrapper">
-                <Navbar />
-                <Section1 /> {/* Head Section */}
-                <Section2 /> {/* Offers or searchbar */}
-                <Section3 /> {/* Nearby Services Cards */}
-                <Section4 /> {/* Some info/reviews */}
-            </div>
-            <Footer />
+            <Section1 /> {/* Head Section */}
+            <Section2 /> {/* Offers or searchbar */}
+            <Section3 /> {/* Nearby Services Cards */}
+            <Section4 /> {/* Some info/reviews */}
         </div>
     );
 };
