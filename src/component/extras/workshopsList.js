cae2c4d4d5d035899990d9049730aef97df9c372
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-15 15:32:20 +0530

    Created workshops list

---
+++ src/component/extras/workshopsList.js
@@ -0,0 +1,212 @@
+export const data = [
+    {
+        name: "Auto marc",
+        address:
+            "A-55, Moolchand Shopping Complex, Ring Rd, Defence Colony, New Delhi, Delhi 110024",
+        services: {
+            ps: [
+                {
+                    title: "basic service",
+                    price: "5499",
+                    time: "6 hours",
+                },
+                {
+                    title: "standard service",
+                    price: "7599",
+                    time: "7 hours",
+                },
+                {
+                    title: "comprehensive service",
+                    price: "10900",
+                    time: "8 hours",
+                },
+            ],
+            denting_and_painting: [
+                "full body repaint",
+                "panel denting & repaint",
+                "panel replacement",
+                "car restoration",
+            ],
+            batteries: [
+                {
+                    brand: "Amron",
+                    price: "4500",
+                    warranty: "44 months",
+                },
+                {
+                    brand: "Exide",
+                    price: "5600",
+                    warranty: "66 months",
+                },
+                {
+                    brand: "Livguard",
+                    price: "3200",
+                    warranty: "24 months",
+                },
+            ],
+            car_spa: [
+                {
+                    title: "Basic Cleaning",
+                    price: "6499",
+                    time: "45 minutes",
+                },
+                {
+                    title: "Deep All round Cleaning",
+                    price: "9870",
+                    time: "2 hours",
+                },
+                {
+                    title: "Interior Cleaning",
+                    price: "3450",
+                    time: "40 minutes",
+                },
+            ],
+            ac_serivce: [
+                {
+                    title: "Regular Ac-Service",
+                    price: "2400",
+                },
+                {
+                    title: "High Performance Ac-service",
+                    price: "5500",
+                },
+            ],
+        },
+    },
+    {
+        name: "Go Mechanic",
+        address:
+            "D-1, Satguru Ram Singh Rd, Mayapuri Industrial Area Phase II, Mayapuri, New Delhi, Delhi 110064",
+        services: {
+            ps: [
+                {
+                    title: "basic service",
+                    price: "6499",
+                    time: "6 hours",
+                },
+                {
+                    title: "standard service",
+                    price: "8699",
+                    time: "7 hours",
+                },
+                {
+                    title: "comprehensive service",
+                    price: "11900",
+                    time: "8 hours",
+                },
+            ],
+            denting_and_painting: [
+                "full body repaint",
+                "panel denting & repaint",
+                "panel replacement",
+            ],
+            batteries: [
+                {
+                    brand: "Exide",
+                    price: "5600",
+                    warranty: "66 months",
+                },
+                {
+                    brand: "Livguard",
+                    price: "3200",
+                    warranty: "24 months",
+                },
+            ],
+            car_spa: [
+                {
+                    title: "Basic Cleaning",
+                    price: "6499",
+                    time: "45 minutes",
+                },
+                {
+                    title: "Deep All round Cleaning",
+                    price: "9870",
+                    time: "2 hours",
+                },
+                {
+                    title: "Interior Cleaning",
+                    price: "3450",
+                    time: "40 minutes",
+                },
+                {
+                    title: "Interior Dry cleaning",
+                    price: "9800",
+                    time: "2 hours",
+                },
+            ],
+            ac_serivce: [
+                {
+                    title: "Regular Ac-Service",
+                    price: "1750",
+                },
+                {
+                    title: "High Performance Ac-service",
+                    price: "4900",
+                },
+            ],
+        },
+    },
+    {
+        name: "M S Auto Service",
+        address:
+            "D-1, Satguru Ram Singh Rd, Mayapuri Industrial Area Phase II, Mayapuri, New Delhi, Delhi 110064",
+        services: {
+            ps: [
+                {
+                    title: "basic service",
+                    price: "2500",
+                    time: "2 hours",
+                },
+            ],
+            denting_and_painting: [
+                "full body repaint",
+                "panel denting & repaint",
+                "panel replacement",
+            ],
+            batteries: [
+                {
+                    brand: "Exide",
+                    price: "5600",
+                    warranty: "66 months",
+                },
+                {
+                    brand: "Livguard",
+                    price: "3200",
+                    warranty: "24 months",
+                },
+            ],
+            car_spa: [
+                {
+                    title: "Basic Cleaning",
+                    price: "6499",
+                    time: "45 minutes",
+                },
+                {
+                    title: "Deep All round Cleaning",
+                    price: "9870",
+                    time: "2 hours",
+                },
+                {
+                    title: "Interior Cleaning",
+                    price: "3450",
+                    time: "40 minutes",
+                },
+                {
+                    title: "Interior Dry cleaning",
+                    price: "9800",
+                    time: "2 hours",
+                },
+            ],
+            ac_serivce: [
+                {
+                    title: "Regular Ac-Service",
+                    price: "1750",
+                },
+                {
+                    title: "High Performance Ac-service",
+                    price: "4900",
+                },
+            ],
+        },
+    },
+];
