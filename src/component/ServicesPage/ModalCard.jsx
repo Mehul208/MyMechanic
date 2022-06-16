6e61196594b858284d20772ba3ac132cd10cd69b
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-16 12:31:06 +0530

    created & fixed add and remove from cart

--- src/component/ServicesPage/ModalCard.jsx
+++ src/component/ServicesPage/ModalCard.jsx
@@ -1,16 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import LoadingButton from "@mui/lab/LoadingButton";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import RemoveShoppingCartIcon from "@mui/icons-material/RemoveShoppingCart";
-const ModalCard = ({ name, title, price, time }) => {
+import { cartActions } from "../../store/cart-slice";
+
+const ModalCard = ({ id, name, title, price, time }) => {
     const [loading, setLoading] = useState(false);
     const [inCart, setInCart] = useState(false);
-    const handleClick = () => {
+    const dispatch = useDispatch();
+    const cartList = useSelector((state) => state.cart.cartItemsList);
+
+    useEffect(() => {
+        const checkCart = async () => {
+            const existing = cartList.find(
+                (item) =>
+                    item.id === id &&
+                    item.workshop_name === name &&
+                    item.title === title
+            );
+            if (existing) setInCart(true);
+            else setInCart(false);
+        };
+        checkCart();
+    }, [cartList, id, name, title]);
+
+    const handleAdd = () => {
         setLoading(true);
+        dispatch(
+            cartActions.addToCart({
+                id: id,
+                name,
+                title,
+                price: parseInt(price, 10),
+                time,
+                date: "15 Jun 2022",
+            })
+        );
         setTimeout(() => {
             setLoading(false);
-            setInCart(!inCart);
-        }, 1500);
+            setInCart(true);
+        }, 1000);
+    };
+    const handleRemove = () => {
+        setLoading(true);
+        dispatch(cartActions.removeFromCart(id));
+        setTimeout(() => {
+            setLoading(false);
+            setInCart(false);
+        }, 1000);
     };
     return (
         <div>
@@ -19,7 +57,7 @@ const ModalCard = ({ name, title, price, time }) => {
             price - {price} <br />
             time - {time}
             <LoadingButton
-                onClick={handleClick}
+                onClick={!inCart ? handleAdd : handleRemove}
                 endIcon={
                     !inCart ? (
                         <AddShoppingCartIcon />
