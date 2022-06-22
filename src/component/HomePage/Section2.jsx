eb00d19a91c5108d3ccc5ea8d55a7fa92415d1e7
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-22 11:51:46 +0530

    offers section

--- src/component/HomePage/Section2.jsx
+++ src/component/HomePage/Section2.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { offers } from "../extras/offers";
+import "./css/section2.css";
 
 const Section2 = () => {
-    return <></>;
+    return (
+        <div className="my-5 _offr1a" >
+            <h2 className="mb-3">Offers for you</h2>
+            <div className="_offr1">
+                {offers.map((offer, i) => (
+                    <div    
+                        key={i}
+                        className="_offr"
+                        // style={{ backgroundImage: `url(${offer.image})` }}
+                    >
+                        <h4>{offer.title}</h4>
+                        {/* <br /> */}
+                        <small>{offer.desc}</small>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Section2;
---
+++ src/component/extras/offers.js
@@ -0,0 +1,25 @@
+export const offers = [
+    {
+        title: "Get 1st Inspection Free",
+        desc: "Redeem your free car home inspection. Valid for first time users.",
+        image: "//",
+        code: "GFIF12X3A",
+    },
+    {
+        title: "40% off",
+        desc: "Get flat 40% off on periodic services now.",
+        image: "//",
+        code: "GFIF40ZA",
+    },
+    {
+        title: "10% Off",
+        desc: "10% off on ac services",
+        image: "//",
+        code: "SUMMER10ME",
+    },
+    {
+        title: "Free Car Wash",
+        desc: "Get your free home car wash, from top rated detailers",
+        image: "//",
+    },
+];
