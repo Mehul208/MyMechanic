63b92c7699fd80b41d2e3f2549f3cd24a0ef4d78
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-22 11:52:31 +0530

    rendered cards from server
    - added dispact and selector
    - fixed css

--- src/component/HomePage/Section4.jsx
+++ src/component/HomePage/Section4.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Section4 = () => {
-    return <div className="text-center">Section4</div>;
+    return (
+        <div>
+            <div className="sec4Text">
+                <h1>Top Rated Workshops Near You</h1>
+                <p>Schedule a pick up today</p>
+            </div>
+            <div></div>
+        </div>
+    );
 };
 
 export default Section4;
