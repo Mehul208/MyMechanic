fc361f71192a20319f0fd6805b32ab153364005d
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-22 11:51:01 +0530

    adedd api linl

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,6 @@ import axios from "axios";
 const url = "http://localhost:5000";
 export const fetchServices = () => axios.get(`${url}/services`);
 export const fetchOptions = (id) => axios.get(`${url}/services/options/${id}`);
+export const loginApi = (data) => axios.post(`${url}/user/login/`, data);
 export const registerUserApi = (userData) =>
     axios.post(`${url}/user/register`, userData);
