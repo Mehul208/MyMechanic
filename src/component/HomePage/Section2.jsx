498bd3768cc2c3dd76352a1960728e48ccbbd7eb
Mehul208 <65774892+Mehul208@users.noreply.github.com>
2022-06-22 15:53:10 +0530

    Offer Section

--- src/component/HomePage/Section2.jsx
+++ src/component/HomePage/Section2.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import { offers } from "../extras/offers";
+import { Stack } from "@mui/material";
 import "./css/section2.css";
 
 const Section2 = () => {
     return (
-        <div className="my-5 _offr1a" >
+        <div className="my-5 _offr1a">
             <h2 className="mb-3">Offers for you</h2>
-            <div className="_offr1">
-                {offers.map((offer, i) => (
-                    <div    
-                        key={i}
-                        className="_offr"
-                        // style={{ backgroundImage: `url(${offer.image})` }}
-                    >
-                        <h4>{offer.title}</h4>
-                        {/* <br /> */}
-                        <small>{offer.desc}</small>
-                    </div>
-                ))}
+            <div className="_offr1b">
+                <Stack spacing={2} direction="row">
+                    {offers.map((offer)=>(
+                        <img src={offer.image} alt="offer" />
+                    ))}
+                </Stack>
             </div>
         </div>
     );
--- src/component/extras/offers.js
+++ src/component/extras/offers.js
@@ -1,25 +1,51 @@
+import image1 from "../../images/ps.jpg";
+import image2 from "../../images/ac.jpg";
+import image3 from "../../images/bat.jpg";
+import image4 from "../../images/clutch.jpg";
+import image5 from "../../images/dap.jpg";
+import image6 from "../../images/rep.jpg";
+import image7 from "../../images/wiper.jpg";
 export const offers = [
     {
         title: "Get 1st Inspection Free",
         desc: "Redeem your free car home inspection. Valid for first time users.",
-        image: "//",
+        image: image1,
         code: "GFIF12X3A",
     },
     {
         title: "40% off",
         desc: "Get flat 40% off on periodic services now.",
-        image: "//",
+        image: image2,
         code: "GFIF40ZA",
     },
     {
-        title: "10% Off",
-        desc: "10% off on ac services",
-        image: "//",
-        code: "SUMMER10ME",
+        title: "40% off",
+        desc: "Get flat 40% off on periodic services now.",
+        image: image3,
+        code: "GFIF40ZA",
     },
     {
-        title: "Free Car Wash",
-        desc: "Get your free home car wash, from top rated detailers",
-        image: "//",
+        title: "40% off",
+        desc: "Get flat 40% off on periodic services now.",
+        image: image4,
+        code: "GFIF40ZA",
+    },
+    {
+        title: "40% off",
+        desc: "Get flat 40% off on periodic services now.",
+        image: image5,
+        code: "GFIF40ZA",
+    },
+    {
+        title: "40% off",
+        desc: "Get flat 40% off on periodic services now.",
+        image: image6,
+        code: "GFIF40ZA",
+    },
+    {
+        title: "40% off",
+        desc: "Get flat 40% off on periodic services now.",
+        image: image7,
+        code: "GFIF40ZA",
     },
 ];
